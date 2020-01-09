c2314d96f54637ac337ef095e4655524c2fba8d3
CoolCodingCat <lisa@korrigane.dev>
2020-01-09 21:11:08 +0100

    more extensive menu + return home + coming soon - not tested on mobile

--- js/app.js
+++ js/app.js
@@ -4,10 +4,28 @@ var app = {
     init: function() {
         console.log('initiated');
         console.log('Et bien dites donc. Vous êtes curieux vous !');
-        $('.goapi').on('click',app.handleClick);
-        $('.answer').addClass("hidden");
+        $('.goapi').on('click',app.handleClickNext);
+        $('.upcoming').on('click',app.handleClickSoon);
+        $('.the-rockets').on('click',app.handleClickSoon);
+        $('.return-home').on('click',app.handleReturnHome);
+        $('.answer').addClass("is-hidden");
+        $('.coming-soon').addClass("is-hidden");
+        $('.return-home').addClass("is-hidden");
+
     },
-    handleClick: function() {
+    handleClickSoon: function() {
+        $('.menu-button').addClass("is-hidden");
+        $(".return-home").removeClass('is-hidden');
+        $(".coming-soon").removeClass('is-hidden');
+    },
+    handleReturnHome: function() {
+        console.log('going home');
+        $('.menu-button').removeClass('is-hidden');
+        $('.menu-section').removeClass("is-hidden");
+        $('.button').removeClass("is-loading");
+        app.init();
+    },
+    handleClickNext: function() {
         $('.button').addClass("is-loading");
 
         app.nextLaunch();
@@ -19,14 +37,16 @@ var app = {
             dataType: 'json',
         });
         launch.done(function(response) {
-
-            $(".answer").removeClass('hidden');
-
-
-            $('.button').remove();
+            
+            $(".answer").removeClass('is-hidden');
+            $(".return-home").removeClass('is-hidden');
 
 
-            /* console.log(response);
+            $('.menu-button').addClass("is-hidden");
+            $('.menu-section').addClass("is-hidden");
+
+            console.log(response);
+            /* 
             console.log(response.details); //details
             console.log(response.crew); //crew
             console.log(response.mission_name); //mission name
@@ -34,12 +54,19 @@ var app = {
             console.log(response.launch_date_utc); //launch date
             console.log(response.rocket.rocket_name); //rocket name
             console.log(response.launch_site.site_name_long); //site name */
+            console.log(response.rocket.second_stage.payloads[0].customers[0]);
+            console.log(response.rocket.second_stage.payloads[0].cargo_manifest);
             
 
             if (!response.crew) {
                 $('#crew_details').append("No crew");
             } else {
                 $('#crew_details').append(response.crew);
+            }
+            if (!response.rocket.second_stage.payloads[0].cargo_manifest) {
+                $('#payload').append("No payload");
+            } else {
+                $('#payload').append(response.rocket.second_stage.payloads[0].cargo_manifest);
             }
 
             $('#name').append(response.mission_name);
@@ -52,6 +79,15 @@ var app = {
             $('#time').append(properDate.slice(11,19));
             $('#rocket').append(response.rocket.rocket_name);
             $('#site').append(response.launch_site.site_name_long);
+            $('#client').append(response.rocket.second_stage.payloads[0].customers[0]);
+
+
+
+            /* for (i=0; i<response.rocket.second_stage.payloads.length; i++) {
+                for (j=0; j<response.rocket.second_stage.payloads[i].customers.length; j++) {
+                        $('#client').append("<li></li>")
+                    }
+                } */
 
 
 
